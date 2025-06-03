5393d950fe264e3cf4e34ec74118c1d9ea23c35e
ohyn <owenorsini@gmail.com>
2025-06-03 02:26:04 -0400

    Water and Sand particles working together

--- sketch.js
+++ sketch.js
@@ -1,187 +1,224 @@
+let grid;
+const ROWS = 80;
+const COLS = 150;
+const CELL_SIZE = 8;
+let hue = 0;
+
 const Type = {
   AIR: 0,
   SAND: 1,
+  WATER: 2,
 };
 
-class Grid {
-  constructor() {
-    this.rows = 60;
-    this.cols = 120;
-    this.cell_size = 20;
-    this.cells = this.createGrid();
-    this.hue = 0;
+function placeParticle(grid, x, y, type, hue = 100, sat = 50) {
+
+  if (x < 0 || x >= ROWS || y < 0 || y >= COLS) {
+    return; // Out of bounds
   }
 
-  createGrid() {
-    let cells = new Array(this.rows);
-    let cell_obj = {
+  if (grid[x][y].type != Type.AIR) {
+    return; // Cell is already occupied
+  }
+  grid[x][y] = {
+    type: type,
+    hue: hue,
+    sat: sat,
+  };
+};
+
+function initializeGrid(rows, cols) {
+  let grid = new Array(rows);
+  for (let row = 0; row < rows; row++) {
+    grid[row] = new Array(cols).fill(null).map(() => ({
       type: Type.AIR,
-      sat: 50,
       hue: 0,
-    };
-    for (let row = 0; row < this.rows; row++) {
-      cells[row] = new Array(this.cols).fill(null).map(() => (
-        { ...cell_obj }
-      ));
-    }
-    return cells;
+      sat: 0,
+    }));
   }
+  return grid;
+}
 
-
-  placeSand(x, y) {
-    if (x < 0 || x >= this.cols || y < 0 || y >= this.rows) {
-      return;
-    }
-    if (this.cells[y][x].type == Type.AIR) {
-      this.cells[y][x].type = Type.SAND;
-      this.cells[y][x].hue = 200;
-      this.cells[y][x].sat = 50;
-    }
+function drawParticle(grid, cellSize, row, col) {
+  if (grid[row][col].type != Type.AIR) {
+    let sat = grid[row][col].sat;
+    let hue = grid[row][col].hue;
+    noStroke();
+    fill(hue, 40 + 2 * row, sat);
+    let y = row * cellSize;
+    let x = col * cellSize;
+    rect(x, y, cellSize, cellSize)
   }
+}
 
-  drawCell(row, col) {
-
-    if (this.cells[row][col].type != Type.AIR) {
-      let sat = this.cells[row][col].sat;
-      let hue = this.cells[row][col].hue;
-      noStroke();
-      fill(hue, 40 + 2 * row, sat,);
-      let y = this.cell_size * row;
-      let x = this.cell_size * col;
-      rect(
-        x,
-        y,
-        this.cell_size,
-        this.cell_size,
-      )
-        .circle(
-          x + this.cell_size / 2,
-          y + this.cell_size / 2,
-          this.cell_size + 2,
-        );
-    }
-  }
-
-  render() {
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.cols; col++) {
-        this.drawCell(row, col);
-      }
-    }
-  }
-
-  changeCell(cell, type, hue, sat) {
-    let new_cell = cell;
-    new_cell.type = type;
-    new_cell.hue = hue;
-    new_cell.sat = sat;
-
-    return new_cell;
-  }
-
-  handleSandPhysics(new_cells, row, col) {
-    let y = row + 1;
-    if (y >= this.rows || this.cells[y][col].type == Type.SAND) {
-      new_cells[row][col] = this.cells[row][col];
-
-      if (y < this.rows && y > 0) {
-        // Check left
-        let fall_left = false;
-        let x_left = col - 1;
-        if (x_left >= 0 && this.cells[y][x_left].type == Type.AIR) {
-          fall_left = true;
-        }
-
-        // Check right
-        let fall_right = false;
-        let x_right = col + 1;
-        if (x_right < this.cols && this.cells[y][x_right].type == Type.AIR) {
-          fall_right = true;
-        }
-
-        // Move the cell left or right depending on neighbors
-        if (fall_left && fall_right) {
-          let x = x_left;
-          if (Math.random() > .5) {
-            x = x_right;
-          }
-          // new_cells[y][x] = this.cells[row][col] + 20;
-          new_cells[y][x] = this.changeCell(
-            this.cells[row][col],
-            Type.SAND,
-            this.hue,
-            this.cells[row][col].sat + 20,
-          );
-          new_cells[row][col] = 0;
-        } else if (fall_left) {
-          new_cells[y][x_left] = this.changeCell(
-            this.cells[row][col],
-            Type.SAND,
-            this.hue,
-            this.cells[row][col].sat + 20,
-          );
-          new_cells[row][col] = { type: Type.AIR, hue: 0, sat: 0 };
-        } else if (fall_right) {
-          new_cells[y][x_right] = this.changeCell(
-            this.cells[row][col],
-            Type.SAND,
-            this.hue,
-            this.cells[row][col].sat + 20,
-          );
-          new_cells[row][col] = { type: Type.AIR, hue: 0, sat: 0 };
-        }
-      }
-    } else {
-      new_cells[y][col] = this.changeCell(
-        this.cells[row][col],
-        Type.SAND,
-        this.hue,
-        this.cells[row][col].sat,
-      );
-      new_cells[row][col] = { type: Type.AIR, hue: 0, sat: 0 };
-    }
-  }
-
-  nextgrid() {
-    let new_cells = this.createGrid();
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.cols; col++) {
-        if (this.cells[row][col].type == Type.SAND) {
-          this.handleSandPhysics(new_cells, row, col);
-        }
-      }
-    }
-    this.cells = new_cells;
-
-  }
-
-  mouse() {
-    if (mouseIsPressed) {
-      let x = floor(mouseX / this.cell_size);
-      let y = floor(mouseY / this.cell_size);
-      this.placeSand(x - 2, y);
-      this.placeSand(x + 3, y);
-      this.placeSand(x, y - 2);
-      this.placeSand(x, y + 3);
-      this.hue += 1;
-      if (this.hue > 360) {
-        this.hue = 1;
-      }
+function drawGrid(grid, cell_size, rows, cols) {
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      drawParticle(grid, cell_size, row, col);
     }
   }
 }
 
-let grid = new Grid();
+function moveParticle(grid, newgrid, from_row, from_col, to_row, to_col, sat = 0) {
+  newgrid[from_row][from_col] = grid[to_row][to_col];
+  newgrid[to_row][to_col] = grid[from_row][from_col];
+  if (sat != 0) {
+    newgrid[to_row][to_col].sat = newgrid[to_row][to_col].sat + sat;
+  }
+
+}
+
+function nextParticle(grid, newgrid, row, col) {
+  let y = row + 1;
+  if (y >= ROWS || grid[y][col].type == Type.SAND) {
+    newgrid[row][col] = grid[row][col];
+
+    if (y < ROWS && y > 0) {
+
+      // Check left
+      let x_left = col - 1;
+      let fall_left =
+        (x_left >= 0 && grid[y][x_left].type != Type.SAND);
+
+      // Check right
+      let x_right = col + 1;
+      let fall_right = (x_right < COLS && grid[y][x_right].type != Type.SAND)
+
+      // Move the cell left or right depending on neighbors
+      if (fall_left && fall_right) {
+        let x = x_left;
+        if (Math.random() > .5) {
+          x = x_right;
+        }
+        moveParticle(grid, newgrid, row, col, y, x, 5);
+      } else if (fall_left) {
+        moveParticle(grid, newgrid, row, col, y, x_left, 5);
+      } else if (fall_right) {
+        moveParticle(grid, newgrid, row, col, y, x_right, 5);
+      }
+    }
+  } else if (grid[y][col].type == Type.AIR) {
+    moveParticle(grid, newgrid, row, col, y, col);
+  } else if (grid[y][col].type == Type.WATER) {
+    moveParticle(grid, newgrid, row, col, y, col, -4);
+
+  } else {
+    // Otherwise, stay in place
+    newgrid[row][col] = grid[row][col];
+  }
+}
+
+function nextWaterParticle(grid, newgrid, row, col) {
+  let y = row + 1;
+
+  if (y >= ROWS || grid[y][col].type == Type.WATER || grid[y][col].type == Type.SAND) {
+    newgrid[row][col] = grid[row][col];
+
+    if (y < ROWS && y > 0) {
+      let ledge = findLedge(grid, row, col);
+      if (ledge) {
+        if (ledge.Direction == Direction.LEFT) {
+          moveParticle(grid, newgrid, row, col, row, col - 1);
+        } else if (ledge.Direction == Direction.RIGHT) {
+          moveParticle(grid, newgrid, row, col, row, col + 1);
+        }
+      }
+    }
+  }
+  else {
+    moveParticle(grid, newgrid, row, col, y, col);
+  }
+}
+
+const Direction = {
+  LEFT: 0,
+  RIGHT: 1,
+}
+
+function findLedge(grid, row, col) {
+  let search_length = 100;
+  for (let i = 1; i <= search_length; i++) {
+    let x = col + i;
+    if (x < 0 || x >= COLS || grid[row][x].type != Type.AIR) {
+      break; // we hit a wall or another particle
+    }
+    if (grid[row + 1][x].type == Type.AIR) {
+      return { Direction: Direction.RIGHT };
+    }
+  }
+
+  for (let i = 1; i <= search_length; i++) {
+
+    let x = col - i;
+    if (x < 0 || x >= COLS || grid[row][x].type != Type.AIR) {
+      break; // we hit a wall or another particle
+    }
+    if (grid[row + 1][x].type == Type.AIR) {
+      return { Direction: Direction.LEFT };
+    }
+  }
+
+  return null;
+}
+
+
+function nextgrid(grid, rows, cols) {
+  let newgrid = initializeGrid(rows, cols);
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (newgrid[row][col].type === Type.AIR) {
+        if (grid[row][col].type == Type.SAND) {
+          nextParticle(grid, newgrid, row, col);
+        } else if (grid[row][col].type == Type.WATER) {
+          nextWaterParticle(grid, newgrid, row, col);
+        }
+      }
+    }
+  }
+  return newgrid;
+
+}
+function mousePressed() {
+  mouseHeld();
+}
+
+function mouseHeld() {
+
+  if (mouseIsPressed) {
+    let x = floor(mouseX / CELL_SIZE);
+    let y = floor(mouseY / CELL_SIZE);
+    // randomly place sand particles left and right of the mouse
+    x += Math.floor(Math.random() * 3) - 1; // -1, 0, or +1
+
+    if (mouseButton === LEFT) {
+      placeParticle(grid, y, x, Type.SAND, hue);
+      hue += .1;
+      if (hue > 160) {
+        hue = 1;
+      }
+    } else {
+      placeParticle(grid, y, x, Type.WATER, 200, 150);
+
+    }
+  }
+}
+
 function setup() {
-  grid = new Grid();
-  createCanvas(grid.cols * grid.cell_size, grid.rows * grid.cell_size);
+  grid = initializeGrid(ROWS, COLS);
+  createCanvas(COLS * CELL_SIZE, ROWS * CELL_SIZE);
   colorMode(HSB, 360, 255, 255);
   frameRate(144);
 }
 
+function keyPressed() {
+  if (key === ' ') {
+    grid = initializeGrid(ROWS, COLS); // Reset the grid
+  }
+}
+
 function draw() {
-  background(200);
-  grid.render();
-  grid.mouse();
-  grid.nextgrid();
+  background(200, 35, 460);
+  drawGrid(grid, CELL_SIZE, ROWS, COLS);
+  mouseHeld();
+  grid = nextgrid(grid, ROWS, COLS);
+
 }
